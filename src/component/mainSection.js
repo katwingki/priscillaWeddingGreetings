4b8a6aae3f26d4bef79ed17feed5c8c8adbd3704
Kathryn Lam <katwingki@gmail.com>
2020-11-06 02:25:59 -0500

    Feature: image changed to url link. SP: deploy again

--- src/component/mainSection.js
+++ src/component/mainSection.js
@@ -12,7 +12,7 @@ function mainSection() {
 
       <h2>㊗️福您们  白头到老 永结同❤️</h2>
 
-      <img src='/images/mainImage.jpeg' alt='text' />
+      <img src='https://res.cloudinary.com/dp8rfxspl/image/upload/v1604647458/mainImage_dqih51.jpg' alt='text' />
       <div>
       <audio src='/audios/CanonInD.mpeg' controls autoPlay loop />
 
